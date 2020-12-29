8572918ecf43791ed4631d4405cd26264e226c48
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2020-12-29 20:10:06 +0100

    Add scripts to facilitate deployment tasks

---
+++ _includes/mongo/k8s/examples/sharding/scripts/gen-cert.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+help () {
+  echo "usage: gen-cert.sh [name] [DN]"
+}
+
+if [ $#  -lt 2 ]; then
+  echo "Illegal number of parameters"
+  help
+  exit 1
+fi
+
+NAME=$1
+DN=$2
+
+# Create Private Key 
+openssl genrsa -out $NAME.key.pem 2048
+
+# Create CSR 
+openssl req -new -out $NAME.csr -key $NAME.key.pem -subj $DN
+
+CSR_BASE64=$(cat $NAME.csr | base64 | tr -d "\n")
+
+echo $CSR_BASE64
+
+# Create K8s CSR manifest
+cat <<EOF | kubectl apply -f -
+apiVersion: certificates.k8s.io/v1beta1
+kind: CertificateSigningRequest
+metadata:
+  name: $NAME-csr
+spec:
+  request: $CSR_BASE64
+EOF
+
+# Approve K8s CSR
+kubectl certificate approve $NAME-csr
+
+# Retrieve certificate
+kubectl get csr/$NAME-csr -o jsonpath='{.status.certificate}{"\n"}' | base64 -d > $NAME.crt
+
+# Concatenate key and certificate
+cat $NAME.key.pem $NAME.crt > $NAME.keycert
